a5325abcfee3835566ce6b45526a90771c35cb31
Bruno Pires <bruno.pires.1096@gmail.com>
2023-10-31 19:50:30 -0300

    chore:configure context

--- src/contexts/crudContext.tsx
+++ src/contexts/crudContext.tsx
@@ -1,5 +1,7 @@
-import React, { ReactNode, createContext, useContext } from 'react';
-import { CrudContextData } from '../types/contexts/crudTypes';
+import React, { ReactNode, createContext, useCallback, useContext, useState } from 'react';
+import { CrudContextData } from '../types/Contexts/crudTypes';
+import { IUser } from '../types/Users';
+import { api } from '../services/axios-config';
 
 
 const CrudContext = createContext({} as CrudContextData)
@@ -9,8 +11,35 @@ interface CrudContextProps {
 }
 
 export const CrudProvider: React.FC<CrudContextProps> = ({ children }) => {
+    const [users, setUsers] = useState<IUser[]>([])
+    // const [userId, setUserId] = useState<IUser>()
+
+
+    const getUsers = useCallback(async () => {
+        try {
+            const response = await api.get('/users')
+            console.log(response.data)
+            setUsers(response.data)
+        } catch (error) {
+
+        } finally {
+
+
+        }
+    }, [])
+
+
+
     return (
-        <CrudContext.Provider value={{}}>
+        <CrudContext.Provider value={{
+            getUsers,
+            // createUser,
+            // updateUser,
+            // getUserById,
+            // deleteUser,
+            users,
+            // userId,
+        }}>
             {children}
         </CrudContext.Provider>
     )
