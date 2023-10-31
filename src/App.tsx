32c11c7123de28d7b5bc5db01e14cbcc6b9d91b6
Bruno Pires <bruno.pires.1096@gmail.com>
2023-10-31 19:50:50 -0300

    chore:config router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import { CrudProvider } from './contexts/crudContext';
+import HomePage from './pages/Home';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <div className="">
-    </div>
+    <CrudProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+      <ToastContainer />
+    </CrudProvider>
   );
 }
 
