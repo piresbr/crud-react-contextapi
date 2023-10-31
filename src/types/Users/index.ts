20370dc080ef761229fc8c84cef0d64ac64fc747
Bruno Pires <bruno.pires.1096@gmail.com>
2023-10-31 19:50:18 -0300

    chore:interfaces user and context

---
+++ src/types/Users/index.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+    dh_registro: string
+    nome: string
+    avatar: string
+    id: string
+}
+
+
--- src/types/contexts/crudTypes.ts
+++ src/types/contexts/crudTypes.ts
@@ -1,3 +1,11 @@
-export interface CrudContextData {
+import { IUser } from "../Users";
 
+export interface CrudContextData {
+    getUsers: () => Promise<void>;
+    // createUser: (formData: IUser) => Promise<void>;
+    // updateUser: (id: number, formData: IUser) => Promise<void>;
+    // getUserById: (id: string) => Promise<void>;
+    // deleteUser: (id: string) => Promise<void>;
+    users: IUser[];
+    // userId: IUser | undefined;
 }
