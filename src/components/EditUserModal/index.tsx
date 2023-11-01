d28a3dce4e38d9a50a5c923e150425af1d4fd9e3
Bruno Pires <bruno.pires.1096@gmail.com>
2023-11-01 11:04:47 -0300

    feat:data on inputs

--- src/components/EditUserModal/index.tsx
+++ src/components/EditUserModal/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { IUser, UsersModalProps } from '../../types/Users';
 import { XCircle } from 'lucide-react';
 
@@ -13,6 +13,20 @@ const EditUserModal: React.FC<EditUserModalProps> = ({
     user,
 }) => {
     console.log(user)
+
+    const nameRef = useRef<HTMLInputElement | null>(null);
+    const avatarRef = useRef<HTMLInputElement | null>(null);
+
+    useEffect(() => {
+        if (nameRef.current) {
+            nameRef.current.value = user.nome
+        }
+        if (avatarRef.current) {
+            avatarRef.current.value = user.avatar
+        }
+    }, [user.avatar, user.nome])
+
+
     return (
         <div className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black/10">
             <div className="flex items-center relative w-full max-w-2xl mx-auto h-full">
@@ -27,8 +41,8 @@ const EditUserModal: React.FC<EditUserModalProps> = ({
                     </div>
 
                     <form className="w-full p-6 space-y-6">
-                        <input type="text" placeholder="Nome: " name="nome" className='w-full py-1 px-3 rounded-md bg-transparent border border-gray-200/50 focus:border-blue-500 focus:outline-none transition-colors' />
-                        <input type="text" placeholder="Link do avatar: " name="avatar" className='w-full py-1 px-3 rounded-md bg-transparent border border-gray-200/50 focus:border-blue-500 focus:outline-none transition-colors' />
+                        <input type="text" ref={nameRef} placeholder="Nome: " name="nome" className='w-full py-1 px-3 rounded-md bg-transparent border border-gray-200/50 focus:border-blue-500 focus:outline-none transition-colors' />
+                        <input type="text" ref={avatarRef} placeholder="Link do avatar: " name="avatar" className='w-full py-1 px-3 rounded-md bg-transparent border border-gray-200/50 focus:border-blue-500 focus:outline-none transition-colors' />
 
                         <div className="flex items-center justify-end py-6 space-x-2 border-t rounded-b border-gray-600">
                             <button type="button" onClick={closeModal} className="focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border text-sm font-medium px-5 py-2.5 focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600">Cancel</button>
